8a04a5139f4647d498106dad1be35da9277e7905
haris-beg <haris.beg@gmail.com>
2018-02-21 16:55:16 -0800

    Revert "Simple Hello World app in React"
    This reverts commit 864ccd51aeed203d9e9f19f1086ffcd8df7d6d76.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Create a new component. This component should produce some HTML.
-const App = function() {
-}
-
-// Take this component's generated HTML and put it on the page (that is, in the DOM).
-ReactDOM.render(React.createElement(App, null), document.querySelector('.container'));
