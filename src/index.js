fc518821b56bf5663ed551d4389ba1db3e5a035f
haris-beg <haris.beg@gmail.com>
2018-02-27 21:03:34 -0800

    fixing a typo (vidoes instead of videos)

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDhW19GlDAB_9QuESJEGQbeGPcAuG1-0N4';
 
-// Create a new component. This component should produce some HTML.
 class App extends Component {
+
   constructor(props) {
     super(props);
-
-    this.state = { vidoes: [] };
-    YTSearch({key: API_KEY, term: 'surfboards'}, (vidoes) => {
-      this.setState = { vidoes };
+    this.state = { videos: [] };
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
     });
   }
+
   render() {
-    return <div><SearchBar /></div>;
+    return (
+        <div>
+          <SearchBar />
+          <VideoList videos={this.state.videos} />
+        </div>
+    );
   }
+
 }
 
 // Take this component's generated HTML and put it on the page (that is, in the DOM).
